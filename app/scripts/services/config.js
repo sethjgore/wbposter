7aca1946d74158de596eb51e3cb30bc24c56738b
sethjgore <sethjgore@gmail.com>
2014-10-17 20:29:16 -0500

    version Monday, ui almost Wednesday

---
+++ app/scripts/services/config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+app.factory('Config', ['', function(){
+
+  var config = {};
+
+  config.drawer.init.background = {'main' : true }
+  config.drawer.init.message = {'green' : true }
+
+  ver Config = {
+    all: config;
+  }
+
+}]);
+
---
+++ app/styles/base/_app.sas
@@ -0,0 +1,9 @@
+.user-select--none, .usn
+  user-select: none
+  -webkit-touch-callout: none
+
+.pointer-events-none, .pen
+  pointer-events: none
+
+*
+  -webkit-tap-highlight-color: rgba(0,0,0,0)
